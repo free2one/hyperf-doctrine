161214332da9bda3ca954dcd42933ffe194a64a8
free2one <free2someone@outlook.com>
2023-06-02 19:18:38 +0800

    feat: 单元测试支持根据扩展自动适配运行

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,12 +8,12 @@ use Hyperf\Di\Definition\DefinitionSourceFactory;
 
 require_once dirname(dirname(__FILE__)) . '/vendor/autoload.php';
 
-// !defined('BASE_PATH') && define('BASE_PATH', dirname(__DIR__, 1) . '/tests/Mock');
 ! defined('BASE_PATH') && define('BASE_PATH', dirname(__DIR__, 1));
-! defined('SWOOLE_HOOK_FLAGS') && define('SWOOLE_HOOK_FLAGS', SWOOLE_HOOK_ALL);
 ! defined('BASE_MOCK_PATH') && define('BASE_MOCK_PATH', dirname(__DIR__, 1) . '/tests/Mock');
-
-Swoole\Runtime::enableCoroutine(true);
+if (extension_loaded('swoole')) {
+    ! defined('SWOOLE_HOOK_FLAGS') && define('SWOOLE_HOOK_FLAGS', SWOOLE_HOOK_ALL);
+    Swoole\Runtime::enableCoroutine(true);
+}
 
 Hyperf\Di\ClassLoader::init(null, BASE_MOCK_PATH . '/App/config/');
 
